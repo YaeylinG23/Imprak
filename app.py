a83fb9f1c076e9817c8becd5724a29cc855d8094
YaeylinG23 <garciayaytube@gmail.com>
2026-01-21 20:06:23 -0600

    Actualización de estilo 0.2

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/nosotros")
+def nosotros():
+    return render_template("nosotros.html")
+
+@app.route("/categorias")
+def categorias():
+    return render_template("categorias.html")
+
+@app.route("/servicios")
+def servicios():
+    return render_template("servicios.html")
+
+@app.route("/mercado")
+def mercado():
+    return render_template("mercado.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
